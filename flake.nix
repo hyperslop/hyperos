b392015bd285072b0c5fcba64178209f2fc5b845
hyperslop <hyperslop@proton.me>
2025-05-10 18:03:39 -0700

    Initial Commit 2

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    hyprland = {
+	url = "github:hyprwm/Hyprland";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    #hyprland-plugins = {
+    #  url = "github:hyprwm/hyprland-plugins";
+    #  inputs.hyprland.follows = "hyprland";
+    #};
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = {self, nixpkgs, ...}@inputs: {
+    # use "nixos", or your hostname as the name of the configuration:
+    # it's a better practice than "default" shown in the video
+#    let
+#    	system = "x86_64-linux";
+#	pkgs = nixpkgs.legacyPackages.${system};
+#    in
+#    {
+#
+#
+#
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+#};
+}
