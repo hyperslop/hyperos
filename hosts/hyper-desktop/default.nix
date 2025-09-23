b493c7c671f4a5e51c087de932bbf101ae1a3a68
hyperslop <hyperslop@proton.me>
2025-09-23 14:18:13 -0700

    fixed hyper-desktop

--- hosts/hyper-desktop/default.nix
+++ hosts/hyper-desktop/default.nix
@@ -9,7 +9,21 @@
 
       ./../../modules/nixos/nixpkgs.nix
       ./../../modules/nixos/flatpak.nix
+
+      inputs.home-manager.nixosModules.default
     ];
+
+
+  home-manager = {
+    useGlobalPkgs = true;    # Use system's nixpkgs
+    useUserPackages = true;
+    extraSpecialArgs = {inherit inputs; };
+    users = {
+      "hyper" = import ./../../users/hyper/home.nix;
+    };
+    #Don't know what this does but it makes home manager work, fixes error message.
+    backupFileExtension = "backup";
+  };
 
   networking.hostName = "pc"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
