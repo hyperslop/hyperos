19499285cbe36aaac94489c8a22ce8b2e6e52f8f
hyperslop <hyperslop@proton.me>
2025-10-02 11:45:46 -0700

    cleaned up broken keyboard shortcuts

--- modules/home-manager/kde.nix
+++ modules/home-manager/kde.nix
@@ -86,39 +86,6 @@
     # Desktop shortcuts for all 8 desktops
     shortcuts = {
       "kwin" = {
-        # Switch between taskbar applications
-        #"Activate Task Manager Entry 1" = "Meta+1";
-        #"Activate Task Manager Entry 2" = "Meta+2";
-        #"Activate Task Manager Entry 3" = "Meta+3";
-        #"Activate Task Manager Entry 4" = "Meta+4";
-        #"Activate Task Manager Entry 5" = "Meta+5";
-        #"Activate Task Manager Entry 6" = "Meta+6";
-        #"Activate Task Manager Entry 7" = "Meta+7";
-        #"Activate Task Manager Entry 8" = "Meta+8";
-        #"Activate Task Manager Entry 9" = "Meta+9";
-        #"Activate Task Manager Entry 10" = "Meta+0";
-
-        # Switch Desktops
-        #"Switch to Desktop 1" = "Meta+Shift+1";
-        #"Switch to Desktop 2" = "Meta+Shift+2";
-        #"Switch to Desktop 3" = "Meta+Shift+3";
-        #"Switch to Desktop 4" = "Meta+Shift+4";
-        #"Switch to Desktop 5" = "Meta+Shift+5";
-        #"Switch to Desktop 6" = "Meta+Shift+6";
-        #"Switch to Desktop 7" = "Meta+Shift+7";
-        #"Switch to Desktop 8" = "Meta+Shift+8";
-        #"Switch to Desktop 9" = "Meta+Shift+8";
-        #"Switch to Desktop 10" = "Meta+Shift+8";
-
-        # Navigate between desktops with arrow keys
-        #"Switch to Next Desktop" = "Meta+Right";
-        #"Switch to Previous Desktop" = "Meta+Left";
-
-
-        # Walk Through Windows
-        #"Walk Through Windows" = "Alt+Tab";
-        #"Walk Through Windows (Reverse)" = "Alt+Shift+Tab";
-        #"Walk Through Windows of Current Application" = "Alt+grave";
       };
     };
   };
