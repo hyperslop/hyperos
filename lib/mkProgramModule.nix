4704aebc3fa4c1cd2d292fcafabf8bc802bfac8c
hyperslop <hyperslop@proton.me>
2025-10-02 16:34:08 -0700

    you can now disable home-manager for a program option with enableHomeManager = false;

--- lib/mkProgramModule.nix
+++ lib/mkProgramModule.nix
@@ -6,20 +6,29 @@
 programName: {
   hasHomeManager ? false,
   extraConfig ? {},
-  packageName ? programName  # Can be overridden or set to null
+  packageName ? programName
 }: { config, lib, pkgs, ... }:
 {
-  options.hyperos.programs.${programName}.enable = lib.mkEnableOption programName;
+  options.hyperos.programs.${programName} = {
+    enable = lib.mkEnableOption programName;
+
+    # NEW: Control home-manager separately
+    enableHomeManager = lib.mkOption {
+      type = lib.types.bool;
+      default = hasHomeManager;  # Default based on whether HM file exists
+      description = "Whether to import home-manager configuration for ${programName}";
+    };
+  };
 
   config = lib.mkIf config.hyperos.programs.${programName}.enable (lib.mkMerge [
-    # Base config - only if packageName is not null
-    (lib.mkIf (packageName != null) {
+    # Base config - only install package if packageName is provided
+    (lib.optionalAttrs (packageName != null) {
       environment.systemPackages = [ (lib.getAttr packageName pkgs) ];
     })
 
-    # Home manager module - only if hasHomeManager is true
-    (lib.mkIf hasHomeManager {
-      home-manager.sharedModules = [ (../modules/home-manager + "/${programName}.nix") ];
+    # Home manager module - only if both hasHomeManager AND enableHomeManager are true
+    (lib.mkIf (hasHomeManager && config.hyperos.programs.${programName}.enableHomeManager) {
+      home-manager.sharedModules = [ (../home-manager + "/${programName}.nix") ];
     })
 
     # Extra custom config
