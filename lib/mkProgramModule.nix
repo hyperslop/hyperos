c40e68fa996eaf59cbd61741c9eb695b109c53ec
hyperslop <hyperslop@proton.me>
2025-10-02 17:34:34 -0700

    fixed nested packages

--- lib/mkProgramModule.nix
+++ lib/mkProgramModule.nix
@@ -8,30 +8,42 @@ programName: {
   extraConfig ? {},
   packageName ? programName
 }: { config, lib, pkgs, ... }:
+let
+  cfg = config.hyperos.programs.${programName};
+
+  # Handle nested packages like "jetbrains.idea-community-bin"
+  getPackageIfExists = name:
+    if name == null then null
+    else lib.attrByPath (lib.splitString "." name) null pkgs;
+
+  package = getPackageIfExists packageName;
+in
 {
   options.hyperos.programs.${programName} = {
     enable = lib.mkEnableOption programName;
 
-    # NEW: Control home-manager separately
     enableHomeManager = lib.mkOption {
       type = lib.types.bool;
-      default = hasHomeManager;  # Default based on whether HM file exists
+      default = hasHomeManager;
       description = "Whether to import home-manager configuration for ${programName}";
     };
   };
 
-  config = lib.mkIf config.hyperos.programs.${programName}.enable (lib.mkMerge [
-    # Base config - only install package if packageName is provided
-    (lib.optionalAttrs (packageName != null) {
-      environment.systemPackages = [ (lib.getAttr packageName pkgs) ];
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    (lib.mkIf (package != null) {
+      environment.systemPackages = [ package ];
     })
 
-    # Home manager module - only if both hasHomeManager AND enableHomeManager are true
-    (lib.mkIf (hasHomeManager && config.hyperos.programs.${programName}.enableHomeManager) {
-      home-manager.sharedModules = [ (../home-manager + "/${programName}.nix") ];
+    (lib.mkIf (packageName != null && package == null) {
+      warnings = [
+        "Package '${packageName}' not found for program '${programName}'"
+      ];
     })
 
-    # Extra custom config
+    (lib.mkIf (hasHomeManager && cfg.enableHomeManager) {
+      home-manager.sharedModules = [ (../modules/home-manager + "/${programName}.nix") ];
+    })
+
     extraConfig
   ]);
 }
