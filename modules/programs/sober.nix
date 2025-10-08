d5b76407a6a29abd320b80d1886ccd12d9696b73
hyperslop <hyperslop@proton.me>
2025-10-07 17:14:36 -0700

    fixed flatpak

--- modules/programs/sober.nix
+++ modules/programs/sober.nix
@@ -2,6 +2,7 @@
 { config, lib, ... }:
 {
   config = lib.mkIf config.hyperos.programs.sober.enable {
+    services.flatpak.enable = true;
     services.flatpak.packages = [
       { appId = "org.vinegarhq.Sober"; origin = "flathub"; }
     ];
--- modules/programs/sonobus-flatpak.nix
+++ modules/programs/sonobus-flatpak.nix
@@ -1,6 +1,7 @@
 { config, lib, ... }:
 {
   config = lib.mkIf config.hyperos.programs.sonobus-flatpak.enable {
+    services.flatpak.enable = true;
     services.flatpak.packages = [
       { appId = "net.sonobus.SonoBus"; origin = "flathub"; }
     ];
